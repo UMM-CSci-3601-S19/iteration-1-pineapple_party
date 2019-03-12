2c28e9510e4733cab53e58dee132b75fb3dc2cb6
teklu009 <teklu009@morris.umn.edu>
2019-03-12 13:46:26 -0500

    Fix typo in add-ride.component.html Add entryComponents to app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -34,7 +34,9 @@ import {AddRideComponent} from "./rides/add-ride.component";
     RideListService,
     {provide: APP_BASE_HREF, useValue: '/'},
   ],
-
+  entryComponents: [
+    AddRideComponent,
+  ],
   bootstrap: [AppComponent]
 })
 
